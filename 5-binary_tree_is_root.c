70a42842372f2152406bf5244f94e0caba1ffa7d
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-08-01 01:13:11 +0100

    root and leaf

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+* binary_tree_is_root - A function that checks if a
+* node is a root
+* @node: Pointer to the node to check
+* Return: 1 if node, else 0
+* if node is NUUL , return 0
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+        if (node == NULL)
+        {
+                return (0);
+        }
+        if (node->parent == NULL)
+        {
+                return (1);
+        }
+        
+        return (0);
+}
