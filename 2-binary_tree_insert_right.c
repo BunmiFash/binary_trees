0a4f891323b9c5b51f7c5808171e5116b131a04b
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-08-02 02:10:52 -0700

    declare

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 	binary_tree_t *tmp;
 
-	binary_tree_node(parent, value);
+	node = binary_tree_node(parent, value);
 
 	if (parent->right == NULL)
 	{
