b7d3d00ab391f990fa4622eb339fd6a2db6964b8
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-08-03 08:44:07 -0700

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,10 +11,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t leaves, left, right;
 
 	if (tree->left == NULL && tree->right == NULL)
-        {
+	{
 		leaves = 1;
 		return (leaves);
-}
+	}
 
 	if (tree == NULL)
 	{
