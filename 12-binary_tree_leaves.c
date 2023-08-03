c478e7948a6a7cec242daf1b366d4b5266fd7932
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-08-03 11:53:00 +0100

    leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,11 +14,16 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		leaves = 1;
+		return (leaves);
+	}
 	if (tree->left == NULL)
 	{
 		left = 0;
 	}
-	else
+	else if (tree->left)
 	{
 		left = 1;
 	}
@@ -26,14 +31,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		right = 0;
 	}
-	else
+	else if (tree->right)
 	{
 		right = 1;
 	}
 	leaves = left + right;
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		leaves = 1;
-	}
 	return (leaves);
 }
